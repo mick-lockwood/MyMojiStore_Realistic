78e315667b5296004612bc907258dd66e47848a6
mick-lockwood <mick.lockwood87@gmail.com>
2026-05-02 19:58:16 +0930

    Update main.js

--- js/main.js
+++ js/main.js
@@ -400,8 +400,8 @@ function setupBinder(scene, overlay) {
 
         // 4. DRAW THE 9x2 SPREAD GRID
         // These coordinates are tuned to align with a standard centered binder graphic
-        let startX = 170; // <>
-        let startY = 180; // ^v
+        let startX = 150; // <>
+        let startY = 300; // ^v
         let spacingX = 125; 
         let spacingY = 155; 
         let spineGap = 115;  // Extra distance added only when jumping to the right page
