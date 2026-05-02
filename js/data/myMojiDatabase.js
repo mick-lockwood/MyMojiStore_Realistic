a51a4c13d48b6b7873ad65eec2e88e9bb8d7726f
mick-lockwood <mick.lockwood87@gmail.com>
2026-05-02 20:13:12 +0930

    Update myMojiDatabase.js

--- js/data/myMojiDatabase.js
+++ js/data/myMojiDatabase.js
@@ -1,21 +1,43 @@
 const myMojiDatabase = [
-    // --- COMMONS ---
+    // --- COMMONS (20 Cards - Enough to test pagination!) ---
     { id: "m_001", name: "Classic Smile", rarity: "Common", baseValue: 1.50 },
     { id: "m_002", name: "Sweat Drop", rarity: "Common", baseValue: 1.25 },
     { id: "m_003", name: "Thumbs Up", rarity: "Common", baseValue: 1.00 },
     { id: "m_004", name: "Sleepy Zzz", rarity: "Common", baseValue: 1.50 },
     { id: "m_005", name: "Slight Frown", rarity: "Common", baseValue: 0.50 },
+    { id: "m_006", name: "Crying Laugh", rarity: "Common", baseValue: 1.50 },
+    { id: "m_007", name: "Winking Face", rarity: "Common", baseValue: 1.25 },
+    { id: "m_008", name: "Tongue Out", rarity: "Common", baseValue: 1.00 },
+    { id: "m_009", name: "Nerd Glasses", rarity: "Common", baseValue: 1.50 },
+    { id: "m_010", name: "Thinking Face", rarity: "Common", baseValue: 0.50 },
+    { id: "m_011", name: "Halo Angel", rarity: "Common", baseValue: 1.50 },
+    { id: "m_012", name: "Devil Horns", rarity: "Common", baseValue: 1.25 },
+    { id: "m_013", name: "Masked Face", rarity: "Common", baseValue: 1.00 },
+    { id: "m_014", name: "Sneezing", rarity: "Common", baseValue: 1.50 },
+    { id: "m_015", name: "Dizzy Eyes", rarity: "Common", baseValue: 0.50 },
+    { id: "m_016", name: "Zipper Mouth", rarity: "Common", baseValue: 1.50 },
+    { id: "m_017", name: "Money Mouth", rarity: "Common", baseValue: 1.25 },
+    { id: "m_018", name: "Shushing Face", rarity: "Common", baseValue: 1.00 },
+    { id: "m_019", name: "Yawning Face", rarity: "Common", baseValue: 1.50 },
+    { id: "m_020", name: "Drooling", rarity: "Common", baseValue: 0.50 },
 
-    // --- RARES ---
-    { id: "m_006", name: "Star Eyes", rarity: "Rare", baseValue: 15.00 },
-    { id: "m_007", name: "Cool Glasses", rarity: "Rare", baseValue: 18.50 },
-    { id: "m_008", name: "Party Horn", rarity: "Rare", baseValue: 12.00 },
+    // --- RARES (8 Cards) ---
+    { id: "m_021", name: "Star Eyes", rarity: "Rare", baseValue: 15.00 },
+    { id: "m_022", name: "Cool Glasses", rarity: "Rare", baseValue: 18.50 },
+    { id: "m_023", name: "Party Horn", rarity: "Rare", baseValue: 12.00 },
+    { id: "m_024", name: "Exploding Head", rarity: "Rare", baseValue: 15.00 },
+    { id: "m_025", name: "Robot Face", rarity: "Rare", baseValue: 14.00 },
+    { id: "m_026", name: "Alien Gray", rarity: "Rare", baseValue: 16.50 },
+    { id: "m_027", name: "Ghost Boo", rarity: "Rare", baseValue: 13.00 },
+    { id: "m_028", name: "Skull Bones", rarity: "Rare", baseValue: 11.00 },
 
-    // --- EPICS ---
-    { id: "m_009", name: "Melting Face", rarity: "Epic", baseValue: 85.00 },
-    { id: "m_010", name: "Mind Blown", rarity: "Epic", baseValue: 110.00 },
+    // --- EPICS (4 Cards) ---
+    { id: "m_029", name: "Melting Face", rarity: "Epic", baseValue: 85.00 },
+    { id: "m_030", name: "Mind Blown", rarity: "Epic", baseValue: 110.00 },
+    { id: "m_031", name: "Cosmic Swirl", rarity: "Epic", baseValue: 95.00 },
+    { id: "m_032", name: "Glitch Matrix", rarity: "Epic", baseValue: 105.00 },
 
-    // --- LEGENDARIES ---
-    { id: "m_011", name: "Golden Crown", rarity: "Legendary", baseValue: 500.00 },
-    { id: "m_012", name: "Holographic Ghost", rarity: "Legendary", baseValue: 1200.00 }
+    // --- LEGENDARIES (2 Cards) ---
+    { id: "m_033", name: "Golden Crown", rarity: "Legendary", baseValue: 500.00 },
+    { id: "m_034", name: "Holographic Ghost", rarity: "Legendary", baseValue: 1200.00 }
 ];
