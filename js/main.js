3df9209fc3fe288f478657d8f853d21b82319c38
mick-lockwood <mick.lockwood87@gmail.com>
2026-05-02 20:25:29 +0930

    Update main.js

--- js/main.js
+++ js/main.js
@@ -428,7 +428,7 @@ function setupBinder(scene, overlay) {
         let startX = 152; 
         let startY = 225; 
         let spacingX = 121.5; 
-        let spacingY = 159; 
+        let spacingY = 160; 
         let spineGap = 114;  
 
         let col = 0;
