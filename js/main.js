dd3929e23cb3c5172f8bebb41ed44446bdd459e9
mick-lockwood <mick.lockwood87@gmail.com>
2026-05-02 14:21:44 +0930

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("=== THE NEW UI CODE IS RUNNING AGAIN ===");
+console.log("=== THE NEW UI CODE IS RUNNING ===");
 
 // --- 1. GLOBAL STATE (Put this at the very top!) ---
 let playerMoney = 50.00;
